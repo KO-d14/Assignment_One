f67bb830625e39a0cc0d3a0123330acd3b34bdc3
KO-d14 <ainajk@hawaii.edu>
2023-08-31 13:09:58 -1000

    Assignment #3 Completed Parts 1-6
    - Learned about boolean variables
    -Added some comments on tues (8/29) work

--- Intro to Swift Playground.playground/Contents.swift
+++ Intro to Swift Playground.playground/Contents.swift
@@ -6,6 +6,8 @@ var greeting = "Hello, playground"
 
 //var myFirstVariable = "I have created my first variable "
 //NOTE: var is only used when declaring the variable
+
+//NOTE: code below is DECLARATION ONLY
 var myFirstVariable: String
 
 myFirstVariable = "I have created my first variable "
@@ -14,6 +16,7 @@ myFirstVariable = "I have created my first variable "
 /* NOTE: use this to comment a block of code*/
 // NOTE: use this to comment out a single line of code
 
+//NOTE: code below is DECLARE & ASSIGN
 var mySecondVariable = "and look I added 2 strings together"
 
 /* ------PART 7 STRING CONCATENATION
@@ -23,3 +26,21 @@ var myThirdVariable = myFirstVariable + mySecondVariable
 
 print(myThirdVariable)
 
+/* ******ASSIGNMENT #3: INTRO to TRUE & FALSE****** */
+var appState = false
+
+print(appState)
+
+/* -----PART 4
+   * Create 2 new variables for type int----------*/
+var firstInt: Int
+firstInt = 5
+var secondInt = 8
+
+//We are going to create a statenent that compares the 2 vars and assign that statement to a 3rd var
+var comparison = firstInt < secondInt
+var secondComparison = firstInt > secondInt
+
+// PART 7 ON YOUR OWN PART 1:
+
+// PART 8 ON YOUR OWN PART 2:
