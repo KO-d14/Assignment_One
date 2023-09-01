7483da6afe9da02dfe0b0172fa15d1d1d27f594d
KO-d14 <ainajk@hawaii.edu>
2023-08-31 15:10:45 -1000

    Added Comments for Assignment #2 and #3
    - listed all the parts covered in Assignment #2 & 3 (A#2 & A#3)in documents and wrote it in code
    
    TODO
    * still need to work on A#2 part 9
    * still need to work on A#3 part 7 &8

--- Intro to Swift Playground.playground/Contents.swift
+++ Intro to Swift Playground.playground/Contents.swift
@@ -2,45 +2,78 @@ import UIKit
 
 var greeting = "Hello, playground"
 
-/* */
+// -------ASSIGNMENT #2: INTRO to VARS & STRINGS ------------
+// -------PART 1 SET UP
+// -------PART 2 INTRO TO SWIFT
+// -------PART 3 INTRO TO VAR
+// -------PART 4 DECLARE VAR (LINE 21)
+// -------PART 5 ASSIGN STRING (LINE 23)
 
 //var myFirstVariable = "I have created my first variable "
-//NOTE: var is only used when declaring the variable
 
-//NOTE: code below is DECLARATION ONLY
+/* NOTE:
+   > var is only used when declaring the variable
+   > use /* this to comment a block of code */ (must have open and closing )
+   > use // this to comment out a single line of code
+
+ 
+   > code below is DECLARATION ONLY */
 var myFirstVariable: String
 
 myFirstVariable = "I have created my first variable "
+
+//-------PART 6 ASSIGN VAR TO A DIFFERENT DATA TYPE
 //myFirstVariable = "5"
 
-/* NOTE: use this to comment a block of code*/
-// NOTE: use this to comment out a single line of code
 
 //NOTE: code below is DECLARE & ASSIGN
 var mySecondVariable = "and look I added 2 strings together"
 
-/* ------PART 7 STRING CONCATENATION
+/*-------PART 7 STRING CONCATENATION
 essentially myFirstVariable + mySecondVariable ----- */
 
 var myThirdVariable = myFirstVariable + mySecondVariable
 
 print(myThirdVariable)
 
-/* ******ASSIGNMENT #3: INTRO to TRUE & FALSE****** */
+// -------PART 8 REVIEW TODAY'S TOPIC
+// -------PART 9 ON YOUR OWN!!!! <-- START HERE
+ 
+
+
+
+
+
+// -------PART 10 COMMIT & PUSH VIA GITHUB DESKTOP
+
+
+
+/*-------ASSIGNMENT #3: INTRO to TRUE & FALSE------- */
+// -------PART 1 SETUP
+// -------PART 2 INTRO TO BOOLEAN, DECLARE & ASSIGN
 var appState = false
 
+// ------- PART 3 PRINT
 print(appState)
 
-/* -----PART 4
-   * Create 2 new variables for type int----------*/
+// -------PART 4 COMPARE VARS USING GREATER THAN (>) OR LESS THAN (<)
+// -------Create 2 new variables for type int
 var firstInt: Int
 firstInt = 5
 var secondInt = 8
 
+// -------PART 5 TEST IF COMPARISON RESULTS ARE ACCURATE 
 //We are going to create a statenent that compares the 2 vars and assign that statement to a 3rd var
 var comparison = firstInt < secondInt
 var secondComparison = firstInt > secondInt
 
-// PART 7 ON YOUR OWN PART 1:
+// -------PART 6 REVIEW TODAY'S TOPIC
+// -------PART 7 ON YOUR OWN PART 1: USING "EQUAL TO" OPERATOR <-- START HERE
 
-// PART 8 ON YOUR OWN PART 2:
+
+
+
+
+
+
+// -------PART 8 ON YOUR OWN PART 2: CLONE REPO FROM GITHUB.COM USING GITHUB DESKTOP <--- START HERE
